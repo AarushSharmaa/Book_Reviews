5f152d38a5ce95ba8ccad27865882df4635f830d
Aarush Sharma <aarush.sharma6@gmail.com>
2022-09-25 19:18:24 +0530

    added reviews

--- src/App.js
+++ src/App.js
@@ -19,6 +19,13 @@ const bookDB = {
       author: "Author : Team at NCERT",
       description:
         "Comments : If you are a science student from India, you would probably encounter this book in your high school. With crystal clear explanations in as simple words as possible, this book teaches the concepts of Chemistry with a lot of ease while generating interest for the subject."
+    },
+    {
+      name: "NeoG Level 0 : Web Dev Course",
+      rating: "5/5",
+      author: "Creator : Tanay Pratap",
+      description:
+        "Comments : A free online course that teaches the basics of web development by making students build projects."
     }
   ],
 
@@ -43,15 +50,36 @@ const bookDB = {
       author: "Author : John Green",
       description:
         "Comments : Must read if you are a teenager. If you are not, you may get bored by the overwhelming emotions thrown in the book."
+    },
+    {
+      name: "Alchemist",
+      rating: "4/5",
+      author: "Author : Paul Coelho ",
+      description:
+        "Comments : If you are looking for something outside, you will probably end up always looking. A very deep story around discovering human potential."
     }
   ],
   finance: [
     {
       name: "Psychology of Money",
-      rating: "3/5",
+      rating: "4/5",
       author: "Author : Morgan Housel",
       description:
+        "Comments : Teaches you about long term wealth management by narrating a story involving one boy and two fathers."
+    },
+    {
+      name: "Rich Dad Poor Dad",
+      rating: "2/5",
+      author: "Author : Robert T. Kiyosaki",
+      description:
         "Comments : It will talk to you and teach you about your relationship with money. Covers a lot of observational traits. Would be a good read if you are willing to explore your relationship with money"
+    },
+    {
+      name: "The One-Page Financial Plan",
+      rating: "2.5/5",
+      author: "Author : Carl Richards",
+      description:
+        "Comments : It gives an insight into how you can tackle all the complexities surrounding your money in a single page."
     }
   ]
 };
@@ -60,7 +88,7 @@ export default function App() {
   // useState to initialise genre at Business
   // and setGenre is a function that will update the current genre
   // based on our click
-  const [selectedGenre, setGenre] = useState("finance");
+  const [selectedGenre, setGenre] = useState("fiction");
 
   var profile = "https://about-aarush.netlify.app/";
   function genreClickHandler(genre) {
@@ -69,7 +97,7 @@ export default function App() {
   }
   return (
     <div className="App">
-      <h1> 📚 goodbooks </h1>
+      <h1> 📚 GoodReads </h1>
       <p style={{ fontSize: "smaller" }}>
         Checkout my reviews for books from a range of these genres
       </p>
@@ -124,7 +152,7 @@ export default function App() {
       <br />
       <small>
         {" "}
-        For more such recommendations, contact me by clicking{" "}
+        For more such recommendations and reviews, contact me by clicking{" "}
         <a href={profile} target="_blank">
           Here
         </a>
